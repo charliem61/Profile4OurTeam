4c7030ab11164733406004ec64134810a5a47a1d
Mason Walker <masonwalker30@gmail.com>
2022-04-10 17:16:57 -0700

    added folders, files, tests, classes

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,37 @@
+const Employee = require("../lib/Employee");
+
+test("is the constuctor an object", () => {
+  const newEmployee = new Employee();
+  expect(typeof newEmployee).toBe("object");
+});
+
+test("does the constuctor set a name", () => {
+  const name = "Laurie";
+  const newEmployee = new Employee(name);
+  expect(newEmployee.name).toBe(name);
+});
+test("does the constuctor set an id", () => {
+  const id = 1;
+  const newEmployee = new Employee("Yulii", id);
+  expect(newEmployee.id).toBe(id);
+});
+test("does the contructor set an email", () => {
+  const email = "yulii24@gmail.com";
+  const newEmployee = new Employee("name", 1, email);
+  expect(newEmployee.email).toBe(email);
+});
+test("test the getName function", () => {
+  const name = "Laurie";
+  const newEmployee = new Employee(name);
+  expect(newEmployee.getName()).toBe(name);
+});
+test("test the id function", () => {
+  const id = 1;
+  const newEmployee = new Employee("Yulii", id);
+  expect(newEmployee.getId()).toBe(id);
+});
+test("test the email function", () => {
+  const email = "yulii24@gmail.com";
+  const newEmployee = new Employee("name", 1, email);
+  expect(newEmployee.getEmail()).toBe(email);
+});
